531815b038f5b2f03636b0a1d5c983cb21eca689
Hector Garza <hggumich@gmail.com>
2022-09-30 09:03:07 -0400

    create a mutable struct

---
+++ defining_instantiating_structs/target/debug/defining_instantiating_structs.d
@@ -0,0 +1,1 @@
+C:\Users\hectorg\Documents\projects\rust_lang\rust_lang_chapter5\defining_instantiating_structs\target\debug\defining_instantiating_structs.exe: C:\Users\hectorg\Documents\projects\rust_lang\rust_lang_chapter5\defining_instantiating_structs\src\main.rs
---
+++ defining_instantiating_structs/target/debug/deps/defining_instantiating_structs.d
@@ -0,0 +1,5 @@
+C:\Users\hectorg\Documents\projects\rust_lang\rust_lang_chapter5\defining_instantiating_structs\target\debug\deps\defining_instantiating_structs.exe: src\main.rs
+
+C:\Users\hectorg\Documents\projects\rust_lang\rust_lang_chapter5\defining_instantiating_structs\target\debug\deps\defining_instantiating_structs.d: src\main.rs
+
+src\main.rs:

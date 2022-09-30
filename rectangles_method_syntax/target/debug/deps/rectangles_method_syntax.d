f0756f231ec6b9f1c601b8c85000acbe017d0ec6
Hector Garza <hggumich@gmail.com>
2022-09-30 15:52:20 -0400

    finish rectangle with methods

---
+++ rectangles_method_syntax/target/debug/deps/rectangles_method_syntax.d
@@ -0,0 +1,5 @@
+C:\Users\hectorg\Documents\projects\rust_lang\rust_lang_chapter5\rectangles_method_syntax\target\debug\deps\rectangles_method_syntax.exe: src\main.rs
+
+C:\Users\hectorg\Documents\projects\rust_lang\rust_lang_chapter5\rectangles_method_syntax\target\debug\deps\rectangles_method_syntax.d: src\main.rs
+
+src\main.rs:
---
+++ rectangles_method_syntax/target/debug/rectangles_method_syntax.d
@@ -0,0 +1,1 @@
+C:\Users\hectorg\Documents\projects\rust_lang\rust_lang_chapter5\rectangles_method_syntax\target\debug\rectangles_method_syntax.exe: C:\Users\hectorg\Documents\projects\rust_lang\rust_lang_chapter5\rectangles_method_syntax\src\main.rs

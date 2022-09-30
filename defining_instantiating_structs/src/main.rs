d64e8acb6d3bfb7fcb43d299477514a72802c23a
Hector Garza <hggumich@gmail.com>
2022-09-30 10:57:22 -0400

    use a tuple strcts

--- defining_instantiating_structs/src/main.rs
+++ defining_instantiating_structs/src/main.rs
@@ -7,6 +7,19 @@ fn main() {
     };
 
     user1.email = String::from("anotheremail@example.com");
+
+    let user2 =User {
+        active: user1.active,
+        username: user1.username,
+        email: String::from("another@example.com"),
+        sign_in_count: user1.sign_in_count,
+    };
+
+    // same as above
+    // let user2 = User {
+    //   email: String::from("another@example.com"),
+    //   ..user1;
+    // };    
 }
 
 struct User {
@@ -14,4 +27,13 @@ struct User {
     username: String,
     email: String,
     sign_in_count: u64,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email, // same as email: email,
+        username, // same as username: username,
+        active: true,
+        sign_in_count: 1,
+     }
 }
